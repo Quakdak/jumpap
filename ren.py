ba68a4fd199f1da935fe8cbaed6dfdbb61b0bc7b
Akhunzianov <feel.and.drop@mail.ru>
2021-12-30 19:22:06 +0300

    ТЗ

---
+++ ren.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import pygame
+
+pygame.init()
+size = width, height = 500, 300
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('a')
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+class Button:
+    image = load_image("btn1.png")
+    image2 = load_image("btn2.png")
+
+    def __init__(self):
